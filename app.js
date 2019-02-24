3ae5c1fbcd032fb43f851c6a54605eeaf9e2ed7a
Derek <nightwing917@hotmail.com>
2019-02-24 16:02:56 -0700

    adding event listener on form

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 // This will be ES5 the we will use ES6 in the other file
-console.log('Connected!');
+//console.log('Connected!');
+
+// Book Constructor
+function Book(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+// UI Constructor
+function Ui() {}
+
+
+
+
+// Event Listeners
+document.getElementById('book-form').addEventListener('submit', function(e){
+    console.log('test');
+
+    e.preventDefault();
+});
