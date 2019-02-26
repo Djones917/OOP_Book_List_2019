e13a55b0382580ed1f197d43f98b104dca8546b5
Derek <nightwing917@hotmail.com>
2019-02-25 20:06:02 -0700

    add book to list complete

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // This will be ES5 the we will use ES6 in the other file
 //console.log('Connected!');
 
-// Book Constructor
+/// Book Constructor
 function Book(title, author, isbn) {
     this.title = title;
     this.author = author;
@@ -9,18 +9,49 @@ function Book(title, author, isbn) {
 }
 
 // UI Constructor
-function Ui() {}
+function UI() { }
 
+// Add Book To List
+UI.prototype.addBookToList = function (book) {
+    const list = document.getElementById('book-list');
+    // Create tr element
+    const row = document.createElement('tr');
+    // Insert cols
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X<a></td>
+  `;
 
+    list.appendChild(row);
+}
 
+// Clear Fields
+UI.prototype.clearFields = function () {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+}
 
 // Event Listeners
-document.getElementById('book-form').addEventListener('submit', function(e){
+document.getElementById('book-form').addEventListener('submit', function (e) {
     // Get form values
     const title = document.getElementById('title').value,
-          author = document.getElementById('author').value,
-          isbn = document.getElementById('isbn').value;
-    
-    //console.log(title, author, isbn);      
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value
+
+    // Instantiate book
+    const book = new Book(title, author, isbn);
+
+    // Instantiate UI
+    const ui = new UI();
+
+    // Add book to list
+    ui.addBookToList(book);
+
+    // Clear fields
+    ui.clearFields();
+
     e.preventDefault();
 });
